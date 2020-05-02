531f301994c0be657e50c53c77fa21d6f6a68bbd
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2020-05-03 04:07:52 +0500

    pressing number buttons now play sound

--- Retro Calculator Storyboard/ViewController.swift
+++ Retro Calculator Storyboard/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    
+    var btnSound: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundUrl = URL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOf: soundUrl)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(" Error playing \(err.debugDescription)")
+        }
     }
-
-
+    
+    
+    @IBAction func numberPressed(sender: UIButton) {
+        playSound()
+    }
+    
+    
+    func playSound() {
+        if btnSound.isPlaying {
+            btnSound.stop()
+        }
+        
+        btnSound.play()
+    }
+    
+    
 }
 
